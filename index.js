b9ab2ec962c21cb6004c048fa58e4c4b75b2b141
Ptriciax <148201846+Ptriciax@users.noreply.github.com>
2023-11-09 02:10:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+function subscribe() {
+    var email = document.getElementById("email").value;
+    alert("Subscribe with Email: " + email);
+}
+
+function like() {
+    nzjxjz
+
+}
+function dislike() {
+  haha
+}
+
+document.getElementById("like-button").addEventListener("click", like);
+document.getElementById("dislike-button").addEventListener("click", dislike);
+
